8c3f2d524c57734ccbc72c13c3f6206308e0a899
pannarai <pannarai.st42@gmail.com>
2024-10-28 00:25:21 +0700

    fix: security

--- book_store/settings.py
+++ book_store/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zah2sd2t)h#vbo4g)mfrg1-2yc&ec41#!*%1s9-9u2a*74waf7'
+SECRET_KEY = os.getenv('SECRET_KEY', default='')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
